688ca510f3abbdc10146525c196216b76498eb10
Molignoni Fabio <fabio.molignoni@studenti.unitn.it>
2018-12-17 11:25:03 +0100

    Inserisce scheletro base per adattatore rss

---
+++ rss_adapter/index.js
@@ -0,0 +1,30 @@
+//=============================
+//           IMPORT
+//=============================
+var express = require("express")        // handling server
+var morgan = require('morgan')          // Used for debugging
+var bodyParser = require('body-parser')
+//=============================
+//          SETTINGS
+//=============================
+var app = express();                    // define app
+var port = process.env.PORT || 8081;    // Set port
+var bbc_routes = require('./routes/bbc_routes.js')
+var the_verge_routes = require('./routes/the_verge_routes.js')
+
+app.use(morgan('dev'))                  // Used in dev mode to see the requests the server receive.
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());             // Useful to extract data from a POST
+
+//=============================
+//          ROUTES
+//=============================
+/*
+app.use('/v1/theverge', the_verge_routes)   // set routes
+app.use('/v1/bbc', bbc_routes)   // set routes
+*/
+//=============================
+//        START SERVER
+//=============================
+app.listen(port)                // start server
+console.log('Server started on port ' + port)
