624379fbc3645418fd663e886c44a8ea4b4638d4
Molignoni Fabio <fabio.molignoni@studenti.unitn.it>
2018-12-21 16:10:03 +0100

    unico path per rss

--- rss_adapter/routes/bbc_routes.js
+++ rss_adapter/routes/bbc_routes.js
@@ -2,11 +2,13 @@
 //           IMPORT
 //=============================
 const express = require('express')
-const http = require('http')
 const router = express.Router()
-var convert = require('xml-to-json-promise');
-var endpoint = "http://feeds.bbci.co.uk/news"
+var convert = require('xml-to-json-promise')
+const request = require('request')
 
+var endpoint = "http://feeds.bbci.co.uk/news"
+var pages = ['/world', '/uk', '/business', '/politics',
+    '/health', '/education', '/science_and_environment', '/technology', '/entertainment_and_arts']
 //=============================
 //     SET DEFAULT HEADERS
 //=============================
@@ -16,81 +18,26 @@ router.use(function (req, res, next) {
     next();
 });
 
-//=============================
-//        GET WORLD NEWS
-//=============================
-router.get('/world', (req, res) => {
-    responseBBCJSON('/world/rss.xml', res)
-})
-
-//=============================
-//        GET UK NEWS
-//=============================
-router.get('/uk', (req, res) => {
-    responseBBCJSON('/uk/rss.xml', res)
-})
-
-//=============================
-//     GET BUSINESS NEWS
-//=============================
-router.get('/business', (req, res) => {
-    responseBBCJSON('/business/rss.xml', res)
-})
-
-//=============================
-//     GET POLITICS NEWS
-//=============================
-router.get('/politics', (req, res) => {
-    responseBBCJSON('/politics/rss.xml', res)
-})
-
-//=============================
-//      GET HEALTH NEWS
-//=============================
-router.get('/health', (req, res) => {
-    responseBBCJSON('/health/rss.xml', res)
-})
-
-//=============================
-//    GET EDUCATION NEWS
-//=============================
-router.get('/education', (req, res) => {
-    responseBBCJSON('/education/rss.xml', res)
-})
-
-//=============================
-// GET SCIENCE/ENVIRONMENT NEWS
-//=============================
-router.get('/science_and_environment', (req, res) => {
-    responseBBCJSON('/science_and_environment/rss.xml', res)
-})
-
-//=============================
-//    GET TECHNOLOGY NEWS
-//=============================
-router.get('/technology', (req, res) => {
-    responseBBCJSON('/technology/rss.xml', res)
-})
-
-//=============================
-// GET ENTERTAINMENT/ARTS NEWS
-//=============================
-router.get('/entertainment_and_arts', (req, res) => {
-    responseBBCJSON('/entertainment_and_arts/rss.xml', res)
-})
-
-//=============================
-//     RESPONSE HANDLER
-//   Get the converted json
-//sends the json back to client
-//=============================
-function responseBBCJSON(path, res) {
-    bbcXml2Json(path).then(jsonRSS => {
-        res.status(200).json(jsonRSS)
+router.get("/", (req, res) => {
+    let prom = []
+    for (page of pages) {
+        prom.push(bbcXml2Json(endpoint + page + "/rss.xml"))
+    }
+    Promise.all(prom).then(results => {
+        let finalJson = {}
+        finalJson.title = "BBC - News"
+        let news = []
+        for (x of results) {
+            news = news.concat(x)
+        }
+        finalJson.totalResults = news.length
+        finalJson.news = news
+        res.status(200).json(finalJson)
     }).catch(e => {
-        res.status(500).json({ "errors": [{ "msg": "internal error" }] })
+        console.log(e)
+        res.status(500).send({ "errors": [{ "msg": "Internal error" }] })
     })
-}
+})
 
 //=============================
 //       BBC XML 2 JSON
@@ -102,35 +49,22 @@ function bbcXml2Json(path) {
     return new Promise(function (resolve, reject) {
         xmlPage = ''
         // get xml data
-        http.get(endpoint + path, function (bbcRes) {
-            bbcRes.on("data", function (data) { // create xml file
-                xmlPage += data
-            });
-            bbcRes.on('end', function () {
-                convert.xmlDataToJSON(xmlPage).then(jsonPage => { // parse xml to json
-                    // basic informations            
-                    var resJson = {}
-                    resJson.title = jsonPage.rss.channel[0].title[0]
-                    resJson.link = jsonPage.rss.channel[0].link[0]
-                    resJson.lastModified = new Date(jsonPage.rss.channel[0].lastBuildDate[0])
-                    //create array of news
-                    var news = []
-                    for (let item of jsonPage.rss.channel[0].item) {
-                        currentNews = {}
-                        currentNews.title = item.title[0]
-                        currentNews.body = item.description[0]
-                        currentNews.url = item.link[0]
-                        currentNews.datetime = new Date(item.pubDate[0])
-                        news.push(currentNews)
-                    }
-                    resJson.news = news
-                    resolve(resJson)
-                }).catch(e => {
-                    reject(Error("internal error"))
-                })
+        request(path, function (error, response, body) {
+            convert.xmlDataToJSON(body).then(jsonPage => {
+                var news = []
+                for (let item of jsonPage.rss.channel[0].item) {
+                    currentNews = {}
+                    currentNews.title = item.title[0]
+                    currentNews.body = item.description[0]
+                    currentNews.url = item.link[0]
+                    currentNews.datetime = new Date(item.pubDate[0])
+                    news.push(currentNews)
+                }
+                resolve(news)
+            }).catch(e => {
+                console.log(e)
+                reject(Error("internal error"))
             })
-        }).on('error', function (e) {
-            reject(Error("internal error"))
         })
     })
 }
--- rss_adapter/routes/the_verge_routes.js
+++ rss_adapter/routes/the_verge_routes.js
@@ -2,10 +2,12 @@
 //           IMPORT
 //=============================
 const express = require('express')
-const http = require('https')
+const request = require('request')
 const router = express.Router()
 var convert = require('xml-to-json-promise');
+
 var endpoint = "https://www.theverge.com"
+var pages = ['/google', '/apple', '/apps', '/culture', '/microsoft', '/photography', '/policy', '/web']
 
 //=============================
 //     SET DEFAULT HEADERS
@@ -16,74 +18,27 @@ router.use(function (req, res, next) {
     next();
 });
 
-//=============================
-//       GET GOOGLE NEWS
-//=============================
-router.get('/google', (req, res) => {
-    responseVergeJSON('/google/rss/index.xml', res)
-})
-
-//=============================
-//     GET APPLE NEWS
-//=============================
-router.get('/apple', (req, res) => {
-    responseVergeJSON('/apple/rss/index.xml', res)
-})
-
-//=============================
-//       GET APPS NEWS
-//=============================
-router.get('/apps', (req, res) => {
-    responseVergeJSON('/apps/rss/index.xml', res)
-})
-
-//=============================
-//      GET CULTURE NEWS
-//=============================
-router.get('/culture', (req, res) => {
-    responseVergeJSON('/culture/rss/index.xml', res)
-})
-
-//=============================
-//     GET MICROSOFT NEWS
-//=============================
-router.get('/microsoft', (req, res) => {
-    responseVergeJSON('/microsoft/rss/index.xml', res)
-})
-
-//=============================
-//     GET PHOTOGRAPHY NEWS
-//=============================
-router.get('/photography', (req, res) => {
-    responseVergeJSON('/photography/rss/index.xml', res)
-})
-
-//=============================
-//       GET POLICY NEWS
-//=============================
-router.get('/policy', (req, res) => {
-    responseVergeJSON('/policy/rss/index.xml', res)
-})
-
-//=============================
-//       GET WEB NEWS
-//=============================
-router.get('/web', (req, res) => {
-    responseVergeJSON('/web/rss/index.xml', res)
-})
-
-//=============================
-//     RESPONSE HANDLER
-//   Get the converted json
-//sends the json back to client
-//=============================
-function responseVergeJSON(path, res) {
-    theVergeXml2Json(path).then(jsonRSS => {
-        res.status(200).json(jsonRSS)
+router.get("/", (req, res) => {
+    let prom = []
+    for (page of pages) {
+        prom.push(theVergeXml2Json(endpoint + page + "/rss/index.xml"))
+    }
+    Promise.all(prom).then(results => {
+        console.log(results.length)
+        let finalJson = {}
+        finalJson.title = "The Verge - News"
+        let news = []
+        for (x of results) {
+            news = news.concat(x)
+        }
+        finalJson.totalResults = news.length
+        finalJson.news = news
+        res.status(200).json(finalJson)
     }).catch(e => {
-        res.status(500).json({ "errors": [{ "msg": "internal error" }] })
+        console.log(e)
+        res.status(500).send({ "errors": [{ "msg": "Internal error" }] })
     })
-}
+})
 //=============================
 //     THE VERGE XML 2 JSON
 // Get the xml file from https://<endpoint>/<path>
@@ -92,38 +47,23 @@ function responseVergeJSON(path, res) {
 //=============================
 function theVergeXml2Json(path) {
     return new Promise(function (resolve, reject) {
-        xmlPage = ''
-        // get xml data
-        http.get(endpoint + path, function (vergeRes) {
-            vergeRes.on("data", function (data) { // create xml file
-                xmlPage += data
-            });
-            vergeRes.on('end', function () {
-                convert.xmlDataToJSON(xmlPage).then(jsonPage => { // parse xml to json
-                    var resJson = {}
-                    // basic informations 
-                    resJson.title = jsonPage.feed.title[0]
-                    resJson.link = jsonPage.feed.link[0].$.href
-                    resJson.lastModified = new Date(jsonPage.feed.updated[0])
-                    allNews = []
-                    for (entry of jsonPage.feed.entry) {
-                        actualNews = {}
-                        actualNews.datetime = new Date(entry.published)
-                        actualNews.updated = new Date(entry.updated)
-                        actualNews.title = entry.title[0]
-                        actualNews.body = entry.content[0]._
-                        actualNews.url = entry.link[0].$.href
-                        actualNews.author = entry.author[0].name[0]
-                        allNews.push(actualNews)
-                    }
-                    resJson.news = allNews
-                    resolve(resJson)
-                }).catch(e => {
-                    reject(Error("internal error"))
-                })
+        request(path, function (error, response, body) {
+            convert.xmlDataToJSON(body).then(jsonPage => {
+                allNews = []
+                for (entry of jsonPage.feed.entry) {
+                    actualNews = {}
+                    actualNews.datetime = new Date(entry.updated)
+                    actualNews.title = entry.title[0]
+                    actualNews.body = entry.content[0]._
+                    actualNews.url = entry.link[0].$.href
+                    actualNews.author = entry.author[0].name[0]
+                    allNews.push(actualNews)
+                }
+                resolve(allNews)
+            }).catch(e => {
+                console.log(e)
+                reject(Error("internal error"))
             })
-        }).on('error', function (e) {
-            reject(Error("internal error"))
         })
     })
 }
