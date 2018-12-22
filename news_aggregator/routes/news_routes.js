e227c59b794600fb40c9db8d3ed3605baefac1ef
Molignoni Fabio <fabio.molignoni@studenti.unitn.it>
2018-12-22 12:01:01 +0100

    aggiunge integrazione con NYT

--- news_aggregator/routes/news_routes.js
+++ news_aggregator/routes/news_routes.js
@@ -12,6 +12,7 @@ require('dotenv').config()
 const rss_adapter_endpoint = process.env.RSS_ADAPTER || 'localhost:8081/v1'
 const news_logic_endpoint = process.env.NEWS_LOGIC || 'http://localhost:8082/v1'
 const guardian_key = process.env.GUARDIAN_KEY
+const nyt_key = process.env.NYT_KEY
 const guardian_endpoint = 'https://content.guardianapis.com/search'
 const nyt_endpoint = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
 //=============================
@@ -24,27 +25,81 @@ router.use(function (req, res, next) {
 });
 
 router.post('/', (req, res) => {
+    updateNYTEntries().then(news => {
+        res.json(news)
+    })
 })
 
+function updateNYTEntries() {
+    return new Promise(function (resolve, reject) {
+        request.get({
+            url: nyt_endpoint,
+            qs: {
+                'api-key': nyt_key,
+                'begin_date': parseInt(getNYTDate())
+            },
+        }, function (err, response, body) {
+            console.log("quah")
+            body = JSON.parse(body)
+            let newsToPost = parseNYTNews(body.response.docs)
+            bodyPost = {}
+            bodyPost.source = 'new-york-times'
+            bodyPost.lang = "en"
+            bodyPost.news = newsToPost
+            request.post({
+                url: news_logic_endpoint + "/news",
+                body: bodyPost,
+                json: true
+            }, function (err, res, bod) {
+                resolve(bod)
+            })
 
 
+        })
+    })
+
+}
+
+function parseNYTNews(news) {
+    let results = []
+    for (notizia of news) {
+        let current = {}
+        current.lang = "en"
+        current.url = notizia.web_url
+        current.body = notizia.snippet
+        current.title = notizia.headline.main
+        current.datetime = notizia.pub_date
+        results.push(current)
+    }
+    return results
+
+}
+
+function getNYTDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate() - 1,
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('');
+}
 
 function updateGuardianEntries() {
     return new Promise(function (resolve, reject) {
         request.get(guardian_endpoint + "?api-key=" + guardian_key + "&type=article&page-size=100",
             function (error, response, body) {
                 let news = JSON.parse(body)
-                let newsToPost = []
-                for (notizia of news.response.results) {
-                    newsToPost.push(parseGuardianNews(notizia))
-                }
+                let newsToPost = parseGuardianNews(news.response.results)
                 bodyPost = {}
                 bodyPost.source = 'the-guardian'
                 bodyPost.lang = "en"
                 bodyPost.news = newsToPost
                 request.post({
                     url: news_logic_endpoint + "/news",
-                    body: newsToSend,
+                    body: bodyPost,
                     json: true
                 }, function (err, res, bod) {
                     resolve(bod)
@@ -53,13 +108,17 @@ function updateGuardianEntries() {
     })
 }
 
-function parseGuardianNews(notizia) {
-    let result = {}
-    result.title = notizia.webTitle
-    result.url = notizia.webUrl
-    result.datetime = notizia.webPubblicationDate
-    result.lang = "en"
-    return result
+function parseGuardianNews(news) {
+    let results = []
+    for (notizia of news) {
+        let current = {}
+        current.title = notizia.webTitle
+        current.url = notizia.webUrl
+        current.datetime = notizia.webPubblicationDate
+        current.lang = "en"
+        results.push(current)
+    }
+    return results
 }
 
 function updateBBCEntries() {
