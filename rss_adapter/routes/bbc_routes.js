11369c2abaa89de143746a987f3a669409db267f
Molignoni Fabio <fabio.molignoni@studenti.unitn.it>
2018-12-22 10:04:44 +0100

    rss di nuovo con chiamate singole

--- rss_adapter/routes/bbc_routes.js
+++ rss_adapter/routes/bbc_routes.js
@@ -21,14 +21,14 @@ router.use(function (req, res, next) {
 router.get("/", (req, res) => {
     let prom = []
     for (page of pages) {
-        prom.push(bbcXml2Json(endpoint + page + "/rss.xml"))
+        prom.push(bbcXml2Json(page + "/rss.xml"))
     }
     Promise.all(prom).then(results => {
         let finalJson = {}
         finalJson.title = "BBC - News"
         let news = []
         for (x of results) {
-            news = news.concat(x)
+            news = news.concat(x.news)
         }
         finalJson.totalResults = news.length
         finalJson.news = news
@@ -40,6 +40,104 @@ router.get("/", (req, res) => {
 })
 
 //=============================
+//        GET WORLD NEWS
+//=============================
+router.get('/world', (req, res) => {
+    bbcXml2Json('/world/rss.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//        GET UK NEWS
+//=============================
+router.get('/uk', (req, res) => {
+    bbcXml2Json('/uk/rss.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//     GET BUSINESS NEWS
+//=============================
+router.get('/business', (req, res) => {
+    bbcXml2Json('/business/rss.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//     GET POLITICS NEWS
+//=============================
+router.get('/politics', (req, res) => {
+    bbcXml2Json('/politics/rss.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//      GET HEALTH NEWS
+//=============================
+router.get('/health', (req, res) => {
+    bbcXml2Json('/health/rss.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//    GET EDUCATION NEWS
+//=============================
+router.get('/education', (req, res) => {
+    bbcXml2Json('/education/rss.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+// ZGET SCIENCE/ENVIRONMENT NEWS
+//=============================
+router.get('/science_and_environment', (req, res) => {
+    bbcXml2Json('/science_and_environment/rss.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//    GET TECHNOLOGY NEWS
+//=============================
+router.get('/technology', (req, res) => {
+    bbcXml2Json('/technology/rss.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+// GET ENTERTAINMENT/ARTS NEWS
+//=============================
+router.get('/entertainment_and_arts', (req, res) => {
+    bbcXml2Json('/entertainment_and_arts/rss.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+//=============================
 //       BBC XML 2 JSON
 // Get the xml file from http://<endpoint>/<path>
 // It converts the xml into suitable json
@@ -49,8 +147,10 @@ function bbcXml2Json(path) {
     return new Promise(function (resolve, reject) {
         xmlPage = ''
         // get xml data
-        request(path, function (error, response, body) {
+        request(endpoint + path, function (error, response, body) {
             convert.xmlDataToJSON(body).then(jsonPage => {
+                var result = {}
+                result.title = 'BBC - News'
                 var news = []
                 for (let item of jsonPage.rss.channel[0].item) {
                     currentNews = {}
@@ -60,7 +160,9 @@ function bbcXml2Json(path) {
                     currentNews.datetime = new Date(item.pubDate[0])
                     news.push(currentNews)
                 }
-                resolve(news)
+                result.totalResults = news.length
+                result.news = news
+                resolve(result)
             }).catch(e => {
                 console.log(e)
                 reject(Error("internal error"))
--- rss_adapter/routes/the_verge_routes.js
+++ rss_adapter/routes/the_verge_routes.js
@@ -21,15 +21,14 @@ router.use(function (req, res, next) {
 router.get("/", (req, res) => {
     let prom = []
     for (page of pages) {
-        prom.push(theVergeXml2Json(endpoint + page + "/rss/index.xml"))
+        prom.push(theVergeXml2Json(page + "/rss/index.xml"))
     }
     Promise.all(prom).then(results => {
-        console.log(results.length)
         let finalJson = {}
         finalJson.title = "The Verge - News"
         let news = []
         for (x of results) {
-            news = news.concat(x)
+            news = news.concat(x.news)
         }
         finalJson.totalResults = news.length
         finalJson.news = news
@@ -39,6 +38,96 @@ router.get("/", (req, res) => {
         res.status(500).send({ "errors": [{ "msg": "Internal error" }] })
     })
 })
+
+//=============================
+//       GET GOOGLE NEWS
+//=============================
+router.get('/google', (req, res) => {
+    theVergeXml2Json('/google/rss/index.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//     GET APPLE NEWS
+//=============================
+router.get('/apple', (req, res) => {
+    theVergeXml2Json('/apple/rss/index.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//       GET APPS NEWS
+//=============================
+router.get('/apps', (req, res) => {
+    theVergeXml2Json('/apps/rss/index.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//      GET CULTURE NEWS
+//=============================
+router.get('/culture', (req, res) => {
+    theVergeXml2Json('/culture/rss/index.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//     GET MICROSOFT NEWS
+//=============================
+router.get('/microsoft', (req, res) => {
+    theVergeXml2Json('/microsoft/rss/index.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//     GET PHOTOGRAPHY NEWS
+//=============================
+router.get('/photography', (req, res) => {
+    theVergeXml2Json('/photography/rss/index.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//       GET POLICY NEWS
+//=============================
+router.get('/policy', (req, res) => {
+    theVergeXml2Json('/policy/rss/index.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+//=============================
+//       GET WEB NEWS
+//=============================
+router.get('/web', (req, res) => {
+    theVergeXml2Json('/web/rss/index.xml').then(jsonRSS => {
+        res.status(200).send(jsonRSS)
+    }).catch(e => {
+        res.status(500).send({ "errors": [{ "msg": "internal error" }] })
+    })
+})
+
+
 //=============================
 //     THE VERGE XML 2 JSON
 // Get the xml file from https://<endpoint>/<path>
@@ -47,8 +136,10 @@ router.get("/", (req, res) => {
 //=============================
 function theVergeXml2Json(path) {
     return new Promise(function (resolve, reject) {
-        request(path, function (error, response, body) {
+        request(endpoint + path, function (error, response, body) {
             convert.xmlDataToJSON(body).then(jsonPage => {
+                response = {}
+                response.title = "The Verge - News"
                 allNews = []
                 for (entry of jsonPage.feed.entry) {
                     actualNews = {}
@@ -59,7 +150,9 @@ function theVergeXml2Json(path) {
                     actualNews.author = entry.author[0].name[0]
                     allNews.push(actualNews)
                 }
-                resolve(allNews)
+                response.totalResults = allNews.length
+                response.news = allNews
+                resolve(response)
             }).catch(e => {
                 console.log(e)
                 reject(Error("internal error"))
